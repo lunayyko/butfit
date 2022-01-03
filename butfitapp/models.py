266080143f5cf6e6c73ee1cdcc753e22ddacf0b2
lunayyko <youryoung.ko@gmail.com>
2022-01-03 23:19:16 +0900

    add: 유저 생성 및 등록

--- butfitapp/models.py
+++ butfitapp/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+from django.db                  import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, id, password=None):
+        if not id:
+            raise ValueError('Users must have an id')
+
+        user = self.model(id=id)
+
+        user.set_password(password)
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    pk_id        = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    id           = models.CharField(max_length=50, unique=True)
+    phone_number = models.CharField(max_length=80)
+
+    objects = UserManager()
+
+    USERNAME_FIELD  = 'id'
+
+    def __str__(self):
+        return f'{self.id}'
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+    
+    class Meta:
+        db_table = 'users'
--- butfitapp/views.py
+++ butfitapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth        import get_user_model
 
-# Create your views here.
+from rest_framework             import status
+from rest_framework.viewsets    import GenericViewSet
+from rest_framework.permissions import AllowAny
+from rest_framework.response    import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models                    import User
+from .serializers               import RegisterUserSerializer
+
+class UserGenericViewSet(GenericViewSet):
+    permission_classes = [AllowAny]
+    queryset           = get_user_model()
+    lookup_field       = 'id'
+
+    def create(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        user  = serializer.save()
+        token = RefreshToken.for_user(user)
+        data  = {'refresh': str(token), 'access': str(token.access_token)}
+        return Response(data, status=status.HTTP_200_OK)
