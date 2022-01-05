dd8b087e4f90aa4ea526f47d334f8439b6e1ea57
lunayyko <youryoung.ko@gmail.com>
2022-01-05 16:12:51 +0900

    delete : git ignore제외 전부

--- butfitapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ButfitappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'butfitapp'
--- butfitapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views      import SignupView, SigninView
-
-urlpatterns = [
-    path('/signin', SigninView.as_view()),
-    path('/signup', SignupView.as_view())
-]
